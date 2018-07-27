6bc80369630a74abcfeb6c132734c39501d8ca2a
Maxime Cyr <max_maxime_max@hotmail.com>
2018-07-27 13:55:52 -0400

    Added refund line item missing properties

--- ShopifySharp/Entities/RefundLineItem.cs
+++ ShopifySharp/Entities/RefundLineItem.cs
@@ -38,5 +38,21 @@ namespace ShopifySharp
         /// </summary>
         [JsonProperty("subtotal")]
         public decimal? SubTotal { get; set; }
+
+        /// <summary>
+        /// How this refund line item affects inventory levels. Valid values: 
+        /// no_restock: Refunding these items won't affect inventory. The number of fulfillable units for this line item will remain unchanged. For example, a refund payment can be issued but no items will be returned or made available for sale again.
+        /// cancel: The items have not yet been fulfilled. The canceled quantity will be added back to the available count. The number of fulfillable units for this line item will decrease.
+        /// return: The items were already delivered, and will be returned to the merchant. The returned quantity will be added back to the available count. The number of fulfillable units for this line item will remain unchanged.
+        /// legacy_restock: The deprecated restock property was used for this refund. These items were made available for sale again. This value is not accepted when creating new refunds.
+        /// </summary>
+        [JsonProperty("restock_type")]
+        public string RestockType { get; set; }
+
+        /// <summary>
+        /// he unique identifier of the location where the items will be restocked. Required when restock_type has the value return or cancel.
+        /// </summary>
+        [JsonProperty("location_id")]
+        public long? LocationId { get; set; }
     }
 }
